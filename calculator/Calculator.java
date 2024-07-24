baf6b52cb0188ace7b87245af737ab94c505a0ad
gsruthiflyers <gangapalli.sruthi@flyerssoft.com>
2024-07-24 13:37:06 +0530

    added pr changes

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -2,36 +2,27 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String []args) {
-        int operator, n1, n2;
-        System.out.println("1 - Add \n 2 -Subtract\n 3- Multiply\n 4- divide\n");
-        System.out.print("choose the operator");
-        Scanner sc = new Scanner(System.in);
-        operator = sc.nextInt();
-        System.out.print("Enter first number");
-        n1 = sc.nextInt();
-        System.out.print("Enter second number");
-        n2 = sc.nextInt();
-         int result =0;
-         switch (operator){
-             case 1:
-          result = n1+n2;
-         break;
-             case 2:
-                 result = n1-n2;
-                 break;
-             case 3:
-                 result = n1*n2;
-                 break;
-             case 4:
-                 result = n1/n2;
-                 break;
+    public int operation(int n1, int n2, int operator) {
+        int result = 0;
+        switch (operator) {
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+                result = n1 - n2;
+                break;
+            case 3:
+                result = n1 * n2;
+                break;
+            case 4:
+                result = n1 / n2;
+                break;
 
-             default:
-                 System.out.println("Entered operator is Invalid");
+            default:
+                System.out.println("Entered operator is Invalid");
 
         }
-        System.out.print(result);
+        return result;
     }
-
 }
+
